2c987f67c89044b11b0e308ec39cacce2e55758b
Rollins, Edward <errollins.jr@gmail.com>
2022-05-15 15:17:25 -0400

    mysql db connection to server

---
+++ src/database.js
@@ -0,0 +1,21 @@
+import mysql from 'mysql';
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Jackjack1',
+    database: 'buy-and-sell',
+});
+
+export const db = {
+    connect: () => connection.connect(),
+    query: (queryString, escapedValues) =>
+        new Promise((resolve, reject) => {
+            connection.query(queryString, escapedValues, (error, results, fields) => {
+                if (error) reject (error);
+                resolve({ results, fields });
+            })
+        }),
+    end: () => connection.end(),   
+
+}
