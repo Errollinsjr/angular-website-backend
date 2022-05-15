ff77af6db7edfd13011f15fab767ae8a1531d3ba
Rollins, Edward <errollins.jr@gmail.com>
2022-05-15 17:36:28 -0400

    add uuid package and createNewListings Route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
+import { createNewListingRoute } from "./createNewListing";
 import { getUserListingsRoute } from "./getUserListings";
 import { addViewToListingRoute } from "./addViewToListings";
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
 
 export default [
+    createNewListingRoute,
     getUserListingsRoute,
     addViewToListingRoute,
     getListingRoute,
